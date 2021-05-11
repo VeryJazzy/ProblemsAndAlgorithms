bc91745b0704d9daa5be29676fe7d09409553c12
very jazzy <jackwilliamcastle@outlook.com>
2021-05-11 09:51:24 +0100

    11/05/21

---
+++ src/main/java/Algorithms/TwoSum.java
@@ -0,0 +1,15 @@
+package Algorithms;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int y = i+1; y < nums.length; y++) {
+                if (nums[i] + nums[y] == target) {
+                    return new int[] {i, y};
+                }
+            }
+        }
+        return null;
+    }
+}
