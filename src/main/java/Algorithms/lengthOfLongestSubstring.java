b74942d3fee08c46211ca8c2589961741824b9ef
very jazzy <jackwilliamcastle@outlook.com>
2021-05-12 10:18:09 +0100

    12/05/21

---
+++ src/main/java/Algorithms/lengthOfLongestSubstring.java
@@ -0,0 +1,39 @@
+package Algorithms;
+
+import java.util.ArrayList;
+
+public class lengthOfLongestSubstring {
+    //Given a string s, find the length of the longest substring without repeating characters.
+
+    public int lengthOfLongestSubstring(String s) {
+        ArrayList<Integer> substringLengths = new ArrayList<Integer>();
+
+        for (int i = 0; i < s.length(); i++) {
+            substringLengths.add(findLengthOfCurrentSubstringBeforeItRepeatsAChar(i, s));
+        }
+
+        return findBiggestSubstring(substringLengths);
+    }
+
+    public int findLengthOfCurrentSubstringBeforeItRepeatsAChar(int i, String s) {
+        ArrayList<Character> charList = new ArrayList<Character>();
+
+        for (int y = i; y < s.length(); y++) {
+            if (charList.contains(s.charAt(y))) {
+                break;
+            }
+            charList.add(s.charAt(y));
+        }
+        return charList.size();
+    }
+
+    public int findBiggestSubstring(ArrayList<Integer> substringLengths) {
+        int biggestSubstring = 0;
+        for (int i : substringLengths) {
+            if (i > biggestSubstring) {
+                biggestSubstring = i;
+            }
+        }
+        return biggestSubstring;
+    }
+}
